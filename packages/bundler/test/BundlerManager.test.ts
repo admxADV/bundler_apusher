31664c6f7da977afb7b1379efec01ef6247f6378
Andrei Samokish <andrei.samokish111@gmail.com>
2024-08-24 15:36:31 +0700

    chore

--- packages/bundler/test/BundlerManager.test.ts
+++ packages/bundler/test/BundlerManager.test.ts
@@ -1,174 +1,204 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { assert, expect } from 'chai'
-import { ethers } from 'hardhat'
-import { parseEther } from 'ethers/lib/utils'
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { assert, expect } from "chai";
+import { ethers } from "hardhat";
+import { parseEther } from "ethers/lib/utils";
 
-import { BundlerReputationParams, ReputationManager } from '../src/modules/ReputationManager'
+import { BundlerReputationParams, ReputationManager } from "../src/modules/ReputationManager";
 import {
-  AddressZero,
-  getUserOpHash,
-  packUserOp,
-  UserOperation,
-  deployEntryPoint, IEntryPoint, DeterministicDeployer
-} from '@account-abstraction/utils'
+	AddressZero,
+	getUserOpHash,
+	packUserOp,
+	UserOperation,
+	deployEntryPoint,
+	IEntryPoint,
+	DeterministicDeployer,
+} from "@account-abstraction/utils";
 
-import { ValidationManager, supportsDebugTraceCall } from '@account-abstraction/validation-manager'
-import { MempoolManager } from '../src/modules/MempoolManager'
-import { BundleManager } from '../src/modules/BundleManager'
-import { BundlerConfig } from '../src/BundlerConfig'
-import { TestFakeWalletToken__factory } from '../src/types'
-import { MethodHandlerERC4337 } from '../src/MethodHandlerERC4337'
-import { ExecutionManager } from '../src/modules/ExecutionManager'
-import { EventsManager } from '../src/modules/EventsManager'
-import { createSigner } from './testUtils'
-import { DepositManager } from '../src/modules/DepositManager'
+import { ValidationManager, supportsDebugTraceCall } from "@account-abstraction/validation-manager";
+import { MempoolManager } from "../src/modules/MempoolManager";
+import { BundleManager } from "../src/modules/BundleManager";
+import { BundlerConfig } from "../src/BundlerConfig";
+import { TestFakeWalletToken__factory } from "../src/types";
+import { MethodHandlerERC4337 } from "../src/MethodHandlerERC4337";
+import { ExecutionManager } from "../src/modules/ExecutionManager";
+import { EventsManager } from "../src/modules/EventsManager";
+import { createSigner } from "./testUtils";
+import { DepositManager } from "../src/modules/DepositManager";
 
-describe('#BundlerManager', () => {
-  let bm: BundleManager
+describe("#BundlerManager", () => {
+	let bm: BundleManager;
 
-  let entryPoint: IEntryPoint
+	let entryPoint: IEntryPoint;
 
-  const provider = ethers.provider
-  const signer = provider.getSigner()
+	const provider = ethers.provider;
+	const signer = provider.getSigner();
 
-  before(async function () {
-    entryPoint = await deployEntryPoint(provider)
-    DeterministicDeployer.init(provider)
+	before(async function () {
+		entryPoint = await deployEntryPoint(provider);
+		DeterministicDeployer.init(provider);
 
-    const config: BundlerConfig = {
-      useRip7560Mode: false,
-      beneficiary: await signer.getAddress(),
-      entryPoint: entryPoint.address,
-      gasFactor: '0.2',
-      minBalance: '0',
-      mnemonic: '',
-      network: '',
-      port: '3000',
-      unsafe: !await supportsDebugTraceCall(provider as any, false),
-      autoBundleInterval: 0,
-      autoBundleMempoolSize: 0,
-      maxBundleGas: 5e6,
-      // minstake zero, since we don't fund deployer.
-      minStake: '0',
-      minUnstakeDelay: 0,
-      conditionalRpc: false
-    }
+		const config: BundlerConfig = {
+			useRip7560Mode: false,
+			beneficiary: await signer.getAddress(),
+			entryPoint: entryPoint.address,
+			gasFactor: "0.2",
+			minBalance: "0",
+			mnemonic: "",
+			network: "",
+			port: "3000",
+			unsafe: !(await supportsDebugTraceCall(provider as any, false)),
+			autoBundleInterval: 0,
+			autoBundleMempoolSize: 0,
+			maxBundleGas: 5e6,
+			// minstake zero, since we don't fund deployer.
+			minStake: "0",
+			minUnstakeDelay: 0,
+			conditionalRpc: false,
+		};
 
-    const repMgr = new ReputationManager(provider, BundlerReputationParams, parseEther(config.minStake), config.minUnstakeDelay)
-    const mempoolMgr = new MempoolManager(repMgr)
-    const validMgr = new ValidationManager(entryPoint, config.unsafe)
-    const evMgr = new EventsManager(entryPoint, mempoolMgr, repMgr)
-    bm = new BundleManager(entryPoint, entryPoint.provider as JsonRpcProvider, entryPoint.signer, evMgr, mempoolMgr, validMgr, repMgr, config.beneficiary, parseEther(config.minBalance), config.maxBundleGas, config.conditionalRpc)
-  })
+		const repMgr = new ReputationManager(
+			provider,
+			BundlerReputationParams,
+			parseEther(config.minStake),
+			config.minUnstakeDelay
+		);
+		const mempoolMgr = new MempoolManager(repMgr);
+		const validMgr = new ValidationManager(entryPoint, config.unsafe);
+		const evMgr = new EventsManager(entryPoint, mempoolMgr, repMgr);
+		bm = new BundleManager(
+			entryPoint,
+			entryPoint.provider as JsonRpcProvider,
+			entryPoint.signer,
+			evMgr,
+			mempoolMgr,
+			validMgr,
+			repMgr,
+			config.beneficiary,
+			parseEther(config.minBalance),
+			config.maxBundleGas,
+			config.conditionalRpc
+		);
+	});
 
-  it('#getUserOpHashes', async () => {
-    const userOp: UserOperation = {
-      sender: AddressZero,
-      nonce: 1,
-      signature: '0x03',
-      callData: '0x05',
-      callGasLimit: 6,
-      verificationGasLimit: 7,
-      maxFeePerGas: 8,
-      maxPriorityFeePerGas: 9,
-      preVerificationGas: 10
-    }
+	it("#getUserOpHashes", async () => {
+		const userOp: UserOperation = {
+			sender: AddressZero,
+			nonce: 1,
+			signature: "0x03",
+			callData: "0x05",
+			callGasLimit: 6,
+			verificationGasLimit: 7,
+			maxFeePerGas: 8,
+			maxPriorityFeePerGas: 9,
+			preVerificationGas: 10,
+		};
 
-    const hash = await entryPoint.getUserOpHash(packUserOp(userOp))
-    const bmHash = await bm.getUserOpHashes([userOp])
-    expect(bmHash).to.eql([hash])
-  })
+		const hash = await entryPoint.getUserOpHash(packUserOp(userOp));
+		const bmHash = await bm.getUserOpHashes([userOp]);
+		expect(bmHash).to.eql([hash]);
+	});
 
-  describe('createBundle', function () {
-    let methodHandler: MethodHandlerERC4337
-    let bundleMgr: BundleManager
+	describe("createBundle", function () {
+		let methodHandler: MethodHandlerERC4337;
+		let bundleMgr: BundleManager;
 
-    before(async function () {
-      const bundlerSigner = await createSigner()
-      const _entryPoint = entryPoint.connect(bundlerSigner)
-      const config: BundlerConfig = {
-        useRip7560Mode: false,
-        beneficiary: await bundlerSigner.getAddress(),
-        entryPoint: _entryPoint.address,
-        gasFactor: '0.2',
-        minBalance: '0',
-        mnemonic: '',
-        network: '',
-        port: '3000',
-        unsafe: !await supportsDebugTraceCall(provider as any, false),
-        conditionalRpc: false,
-        autoBundleInterval: 0,
-        autoBundleMempoolSize: 0,
-        maxBundleGas: 5e6,
-        // minstake zero, since we don't fund deployer.
-        minStake: '0',
-        minUnstakeDelay: 0
-      }
-      const repMgr = new ReputationManager(provider, BundlerReputationParams, parseEther(config.minStake), config.minUnstakeDelay)
-      const mempoolMgr = new MempoolManager(repMgr)
-      const validMgr = new ValidationManager(_entryPoint, config.unsafe)
-      const evMgr = new EventsManager(_entryPoint, mempoolMgr, repMgr)
-      bundleMgr = new BundleManager(_entryPoint, _entryPoint.provider as JsonRpcProvider, _entryPoint.signer, evMgr, mempoolMgr, validMgr, repMgr, config.beneficiary, parseEther(config.minBalance), config.maxBundleGas, false)
-      const depositManager = new DepositManager(entryPoint, mempoolMgr, bundleMgr)
-      const execManager = new ExecutionManager(repMgr, mempoolMgr, bundleMgr, validMgr, depositManager)
-      execManager.setAutoBundler(0, 1000)
+		before(async function () {
+			const bundlerSigner = await createSigner();
+			const _entryPoint = entryPoint.connect(bundlerSigner);
+			const config: BundlerConfig = {
+				useRip7560Mode: false,
+				beneficiary: await bundlerSigner.getAddress(),
+				entryPoint: _entryPoint.address,
+				gasFactor: "0.2",
+				minBalance: "0",
+				mnemonic: "",
+				network: "",
+				port: "3000",
+				unsafe: !(await supportsDebugTraceCall(provider as any, false)),
+				conditionalRpc: false,
+				autoBundleInterval: 0,
+				autoBundleMempoolSize: 0,
+				maxBundleGas: 5e6,
+				// minstake zero, since we don't fund deployer.
+				minStake: "0",
+				minUnstakeDelay: 0,
+			};
+			const repMgr = new ReputationManager(
+				provider,
+				BundlerReputationParams,
+				parseEther(config.minStake),
+				config.minUnstakeDelay
+			);
+			const mempoolMgr = new MempoolManager(repMgr);
+			const validMgr = new ValidationManager(_entryPoint, config.unsafe);
+			const evMgr = new EventsManager(_entryPoint, mempoolMgr, repMgr);
+			bundleMgr = new BundleManager(
+				_entryPoint,
+				_entryPoint.provider as JsonRpcProvider,
+				_entryPoint.signer,
+				evMgr,
+				mempoolMgr,
+				validMgr,
+				repMgr,
+				config.beneficiary,
+				parseEther(config.minBalance),
+				config.maxBundleGas,
+				false
+			);
+			const depositManager = new DepositManager(entryPoint, mempoolMgr, bundleMgr);
+			const execManager = new ExecutionManager(repMgr, mempoolMgr, bundleMgr, validMgr, depositManager);
+			execManager.setAutoBundler(0, 1000);
 
-      methodHandler = new MethodHandlerERC4337(
-        execManager,
-        provider,
-        bundlerSigner,
-        config,
-        _entryPoint
-      )
-    })
+			methodHandler = new MethodHandlerERC4337(execManager, provider, bundlerSigner, config, _entryPoint, validMgr);
+		});
 
-    it('should not include a UserOp that accesses the storage of a different known sender', async function () {
-      if (!await supportsDebugTraceCall(ethers.provider, false)) {
-        console.log('WARNING: opcode banning tests can only run with geth')
-        this.skip()
-      }
+		it("should not include a UserOp that accesses the storage of a different known sender", async function () {
+			if (!(await supportsDebugTraceCall(ethers.provider, false))) {
+				console.log("WARNING: opcode banning tests can only run with geth");
+				this.skip();
+			}
 
-      const wallet1 = await new TestFakeWalletToken__factory(signer).deploy(entryPoint.address)
-      const wallet2 = await new TestFakeWalletToken__factory(signer).deploy(entryPoint.address)
+			const wallet1 = await new TestFakeWalletToken__factory(signer).deploy(entryPoint.address);
+			const wallet2 = await new TestFakeWalletToken__factory(signer).deploy(entryPoint.address);
 
-      await wallet1.sudoSetBalance(wallet1.address, parseEther('1'))
-      await wallet1.sudoSetBalance(wallet2.address, parseEther('1'))
-      await wallet2.sudoSetAnotherWallet(wallet1.address)
-      const calldata1 = wallet2.address
-      const calldata2 = '0x'
+			await wallet1.sudoSetBalance(wallet1.address, parseEther("1"));
+			await wallet1.sudoSetBalance(wallet2.address, parseEther("1"));
+			await wallet2.sudoSetAnotherWallet(wallet1.address);
+			const calldata1 = wallet2.address;
+			const calldata2 = "0x";
 
-      const cEmptyUserOp: UserOperation = {
-        sender: AddressZero,
-        nonce: '0x0',
-        signature: '0x',
-        callData: '0x',
-        callGasLimit: '0x0',
-        verificationGasLimit: '0x50000',
-        maxFeePerGas: '0x0',
-        maxPriorityFeePerGas: '0x0',
-        preVerificationGas: '0x50000'
-      }
-      const userOp1: UserOperation = {
-        ...cEmptyUserOp,
-        sender: wallet1.address,
-        callData: calldata1
-      }
-      const userOp2: UserOperation = {
-        ...cEmptyUserOp,
-        sender: wallet2.address,
-        callData: calldata2
-      }
-      await methodHandler.sendUserOperation(userOp1, entryPoint.address)
-      await methodHandler.sendUserOperation(userOp2, entryPoint.address)
+			const cEmptyUserOp: UserOperation = {
+				sender: AddressZero,
+				nonce: "0x0",
+				signature: "0x",
+				callData: "0x",
+				callGasLimit: "0x0",
+				verificationGasLimit: "0x50000",
+				maxFeePerGas: "0x0",
+				maxPriorityFeePerGas: "0x0",
+				preVerificationGas: "0x50000",
+			};
+			const userOp1: UserOperation = {
+				...cEmptyUserOp,
+				sender: wallet1.address,
+				callData: calldata1,
+			};
+			const userOp2: UserOperation = {
+				...cEmptyUserOp,
+				sender: wallet2.address,
+				callData: calldata2,
+			};
+			await methodHandler.sendUserOperation(userOp1, entryPoint.address);
+			await methodHandler.sendUserOperation(userOp2, entryPoint.address);
 
-      const bundle = await bundleMgr.sendNextBundle()
-      await bundleMgr.handlePastEvents()
-      const mempool = bundleMgr.mempoolManager.getSortedForInclusion()
+			const bundle = await bundleMgr.sendNextBundle();
+			await bundleMgr.handlePastEvents();
+			const mempool = bundleMgr.mempoolManager.getSortedForInclusion();
 
-      assert.equal(bundle!.userOpHashes.length, 1)
-      assert.equal(bundle!.userOpHashes[0], getUserOpHash(userOp1, entryPoint.address, await signer.getChainId()))
-      assert.equal(mempool.length, 1)
-      assert.equal(mempool[0].userOp.sender, wallet2.address)
-    })
-  })
-})
+			assert.equal(bundle!.userOpHashes.length, 1);
+			assert.equal(bundle!.userOpHashes[0], getUserOpHash(userOp1, entryPoint.address, await signer.getChainId()));
+			assert.equal(mempool.length, 1);
+			assert.equal(mempool[0].userOp.sender, wallet2.address);
+		});
+	});
+});
--- packages/bundler/test/DebugMethodHandler.test.ts
+++ packages/bundler/test/DebugMethodHandler.test.ts
@@ -1,114 +1,123 @@
-import { ethers } from 'hardhat'
-import { SimpleAccountAPI } from '@account-abstraction/sdk'
-import { Signer, Wallet } from 'ethers'
-import { parseEther } from 'ethers/lib/utils'
-import { expect } from 'chai'
-import { JsonRpcProvider } from '@ethersproject/providers'
+import { ethers } from "hardhat";
+import { SimpleAccountAPI } from "@account-abstraction/sdk";
+import { Signer, Wallet } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import { expect } from "chai";
+import { JsonRpcProvider } from "@ethersproject/providers";
 
 import {
-  DeterministicDeployer,
-  IEntryPoint,
-  SimpleAccountFactory__factory,
-  deployEntryPoint,
-  resolveHexlify
-} from '@account-abstraction/utils'
-import { ValidationManager, supportsDebugTraceCall } from '@account-abstraction/validation-manager'
+	DeterministicDeployer,
+	IEntryPoint,
+	SimpleAccountFactory__factory,
+	deployEntryPoint,
+	resolveHexlify,
+} from "@account-abstraction/utils";
+import { ValidationManager, supportsDebugTraceCall } from "@account-abstraction/validation-manager";
 
-import { DebugMethodHandler } from '../src/DebugMethodHandler'
-import { ExecutionManager } from '../src/modules/ExecutionManager'
-import { BundlerReputationParams, ReputationManager } from '../src/modules/ReputationManager'
-import { BundlerConfig } from '../src/BundlerConfig'
-import { MempoolManager } from '../src/modules/MempoolManager'
-import { BundleManager, SendBundleReturn } from '../src/modules/BundleManager'
-import { MethodHandlerERC4337 } from '../src/MethodHandlerERC4337'
+import { DebugMethodHandler } from "../src/DebugMethodHandler";
+import { ExecutionManager } from "../src/modules/ExecutionManager";
+import { BundlerReputationParams, ReputationManager } from "../src/modules/ReputationManager";
+import { BundlerConfig } from "../src/BundlerConfig";
+import { MempoolManager } from "../src/modules/MempoolManager";
+import { BundleManager, SendBundleReturn } from "../src/modules/BundleManager";
+import { MethodHandlerERC4337 } from "../src/MethodHandlerERC4337";
 
-import { createSigner } from './testUtils'
-import { EventsManager } from '../src/modules/EventsManager'
-import { DepositManager } from '../src/modules/DepositManager'
+import { createSigner } from "./testUtils";
+import { EventsManager } from "../src/modules/EventsManager";
+import { DepositManager } from "../src/modules/DepositManager";
 
-const provider = ethers.provider
+const provider = ethers.provider;
 
-describe('#DebugMethodHandler', () => {
-  let debugMethodHandler: DebugMethodHandler
-  let entryPoint: IEntryPoint
-  let methodHandler: MethodHandlerERC4337
-  let smartAccountAPI: SimpleAccountAPI
-  let signer: Signer
-  const accountSigner = Wallet.createRandom()
+describe("#DebugMethodHandler", () => {
+	let debugMethodHandler: DebugMethodHandler;
+	let entryPoint: IEntryPoint;
+	let methodHandler: MethodHandlerERC4337;
+	let smartAccountAPI: SimpleAccountAPI;
+	let signer: Signer;
+	const accountSigner = Wallet.createRandom();
 
-  before(async () => {
-    signer = await createSigner()
+	before(async () => {
+		signer = await createSigner();
 
-    entryPoint = await deployEntryPoint(provider)
-    DeterministicDeployer.init(provider)
+		entryPoint = await deployEntryPoint(provider);
+		DeterministicDeployer.init(provider);
 
-    const config: BundlerConfig = {
-      useRip7560Mode: false,
-      beneficiary: await signer.getAddress(),
-      entryPoint: entryPoint.address,
-      gasFactor: '0.2',
-      minBalance: '0',
-      mnemonic: '',
-      network: '',
-      port: '3000',
-      unsafe: !await supportsDebugTraceCall(provider as any, false),
-      conditionalRpc: false,
-      autoBundleInterval: 0,
-      autoBundleMempoolSize: 0,
-      maxBundleGas: 5e6,
-      // minstake zero, since we don't fund deployer.
-      minStake: '0',
-      minUnstakeDelay: 0
-    }
+		const config: BundlerConfig = {
+			useRip7560Mode: false,
+			beneficiary: await signer.getAddress(),
+			entryPoint: entryPoint.address,
+			gasFactor: "0.2",
+			minBalance: "0",
+			mnemonic: "",
+			network: "",
+			port: "3000",
+			unsafe: !(await supportsDebugTraceCall(provider as any, false)),
+			conditionalRpc: false,
+			autoBundleInterval: 0,
+			autoBundleMempoolSize: 0,
+			maxBundleGas: 5e6,
+			// minstake zero, since we don't fund deployer.
+			minStake: "0",
+			minUnstakeDelay: 0,
+		};
 
-    const repMgr = new ReputationManager(provider, BundlerReputationParams, parseEther(config.minStake), config.minUnstakeDelay)
-    const mempoolMgr = new MempoolManager(repMgr)
-    const validMgr = new ValidationManager(entryPoint, config.unsafe)
-    const eventsManager = new EventsManager(entryPoint, mempoolMgr, repMgr)
-    const bundleMgr = new BundleManager(entryPoint, entryPoint.provider as JsonRpcProvider, entryPoint.signer, eventsManager, mempoolMgr, validMgr, repMgr,
-      config.beneficiary, parseEther(config.minBalance), config.maxBundleGas, false)
-    const depositManager = new DepositManager(entryPoint, mempoolMgr, bundleMgr)
-    const execManager = new ExecutionManager(repMgr, mempoolMgr, bundleMgr, validMgr, depositManager)
-    methodHandler = new MethodHandlerERC4337(
-      execManager,
-      provider,
-      signer,
-      config,
-      entryPoint
-    )
+		const repMgr = new ReputationManager(
+			provider,
+			BundlerReputationParams,
+			parseEther(config.minStake),
+			config.minUnstakeDelay
+		);
+		const mempoolMgr = new MempoolManager(repMgr);
+		const validMgr = new ValidationManager(entryPoint, config.unsafe);
+		const eventsManager = new EventsManager(entryPoint, mempoolMgr, repMgr);
+		const bundleMgr = new BundleManager(
+			entryPoint,
+			entryPoint.provider as JsonRpcProvider,
+			entryPoint.signer,
+			eventsManager,
+			mempoolMgr,
+			validMgr,
+			repMgr,
+			config.beneficiary,
+			parseEther(config.minBalance),
+			config.maxBundleGas,
+			false
+		);
+		const depositManager = new DepositManager(entryPoint, mempoolMgr, bundleMgr);
+		const execManager = new ExecutionManager(repMgr, mempoolMgr, bundleMgr, validMgr, depositManager);
+		methodHandler = new MethodHandlerERC4337(execManager, provider, signer, config, entryPoint, validMgr);
 
-    debugMethodHandler = new DebugMethodHandler(execManager, eventsManager, repMgr, mempoolMgr)
+		debugMethodHandler = new DebugMethodHandler(execManager, eventsManager, repMgr, mempoolMgr);
 
-    DeterministicDeployer.init(ethers.provider)
-    const accountDeployerAddress = await DeterministicDeployer.deploy(new SimpleAccountFactory__factory(), 0, [entryPoint.address])
+		DeterministicDeployer.init(ethers.provider);
+		const accountDeployerAddress = await DeterministicDeployer.deploy(new SimpleAccountFactory__factory(), 0, [
+			entryPoint.address,
+		]);
 
-    smartAccountAPI = new SimpleAccountAPI({
-      provider,
-      entryPointAddress: entryPoint.address,
-      owner: accountSigner,
-      factoryAddress: accountDeployerAddress
-    })
-    const accountAddress = await smartAccountAPI.getAccountAddress()
-    await signer.sendTransaction({
-      to: accountAddress,
-      value: parseEther('1')
-    })
-  })
+		smartAccountAPI = new SimpleAccountAPI({
+			provider,
+			entryPointAddress: entryPoint.address,
+			owner: accountSigner,
+			factoryAddress: accountDeployerAddress,
+		});
+		const accountAddress = await smartAccountAPI.getAccountAddress();
+		await signer.sendTransaction({
+			to: accountAddress,
+			value: parseEther("1"),
+		});
+	});
 
-  it('should return sendBundleNow hashes', async () => {
-    debugMethodHandler.setBundlingMode('manual')
-    const addr = await smartAccountAPI.getAccountAddress()
-    const op1 = await smartAccountAPI.createSignedUserOp({
-      target: addr,
-      data: '0x'
-    })
-    const userOpHash = await methodHandler.sendUserOperation(await resolveHexlify(op1), entryPoint.address)
-    const {
-      transactionHash,
-      userOpHashes
-    } = await debugMethodHandler.sendBundleNow() as SendBundleReturn
-    expect(userOpHashes).eql([userOpHash])
-    const txRcpt = await provider.getTransactionReceipt(transactionHash)
-    expect(txRcpt.to).to.eq(entryPoint.address)
-  })
-})
+	it("should return sendBundleNow hashes", async () => {
+		debugMethodHandler.setBundlingMode("manual");
+		const addr = await smartAccountAPI.getAccountAddress();
+		const op1 = await smartAccountAPI.createSignedUserOp({
+			target: addr,
+			data: "0x",
+		});
+		const userOpHash = await methodHandler.sendUserOperation(await resolveHexlify(op1), entryPoint.address);
+		const { transactionHash, userOpHashes } = (await debugMethodHandler.sendBundleNow()) as SendBundleReturn;
+		expect(userOpHashes).eql([userOpHash]);
+		const txRcpt = await provider.getTransactionReceipt(transactionHash);
+		expect(txRcpt.to).to.eq(entryPoint.address);
+	});
+});
